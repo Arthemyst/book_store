4d7a52fe5b2f5009cedb9ce9913e99b86c644c7e
Arthemyst <pawel.lukmin@gmail.com>
2022-05-19 21:38:19 +0200

    Feat: Created Book model.

--- src/bookstore/models.py
+++ src/bookstore/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    external_id = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    authors = models.JSONField()
+    acquired = models.BooleanField()
+    published_year = models.IntegerField()
+    thumbnail = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.title
