b5669d353cac28b696911b21060da3a731b3a142
Arthemyst <pawel.lukmin@gmail.com>
2022-05-26 23:40:10 +0200

    Fix: Added "/" to api_spec page.

--- src/bookstore/urls.py
+++ src/bookstore/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("books/", views.BookList.as_view()),
     path("books/<int:pk>/", views.BookDetail.as_view()),
     path("import/", views.book_import),
-    path("api_spec", views.api_spec),
+    path("api_spec/", views.api_spec),
 ]
