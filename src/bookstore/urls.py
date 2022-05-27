138a891cd7725f9b2859ae1739e11c9f0cd04750
Arthemyst <pawel.lukmin@gmail.com>
2022-05-27 10:51:20 +0200

    make it better

--- src/bookstore/urls.py
+++ src/bookstore/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from bookstore import views
 
+
 urlpatterns = [
     path("books/", views.BookList.as_view()),
     path("books/<int:pk>/", views.BookDetail.as_view()),
