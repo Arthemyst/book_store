f4c5a81adfc3779f077f338430a4201025ed0e18
Pawel <pawel.lukmin@gmail.com>
2022-08-10 19:58:23 +0200

    Fix: test updated

--- src/bookstore/tests/test_book_import.py
+++ src/bookstore/tests/test_book_import.py
@@ -11,13 +11,7 @@ def test_import_books_by_author_valid():
     response1 = save_books_to_db(books, author_valid)
     response2 = save_books_to_db(books, author_valid)
     assert response2 == 0
-    book1 = Book.objects.get(title="Blask fantastyczny Swiat dysku")
-    book2 = Book.objects.get(title="Ostatni kontynent")
-    book3 = Book.objects.get(title="Nauka Świata Dysku")
 
-    book1.delete()
-    book2.delete()
-    book3.delete()
 
 
 def test_import_books_by_author_invalid():
